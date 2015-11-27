26e7549cdf590b627c08f57c4664468cb9a0fb6f
redgieofpuppets <redgieofpuppets@hotmail.com>
2015-11-27 14:30:42 +0000

    Setup

---
+++ guitartuner/guitartuner/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  guitartuner
+//
+//  Created by 20064234 on 27/11/2015.
+//  Copyright © 2015 WIT. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
